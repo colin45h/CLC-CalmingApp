b234fd27e5f8ec4adcc9ff27b0160275edc848b0
Colin Hehn <colin45h@yahoo.com>
2020-12-07 10:58:29 -0600

    Initial Commit

---
+++ CLC_CalmingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CLC_CalmingApp
+//
+//  Created by Tiger Coder on 12/7/20.
+//  Copyright © 2020 clc.hehn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CLC_CalmingAppTests/CLC_CalmingAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CLC_CalmingAppTests.swift
+//  CLC_CalmingAppTests
+//
+//  Created by Tiger Coder on 12/7/20.
+//  Copyright © 2020 clc.hehn. All rights reserved.
+//
+
+import XCTest
+@testable import CLC_CalmingApp
+
+class CLC_CalmingAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
