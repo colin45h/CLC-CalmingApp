bbf4f4825b8604cd6651f929175a5a5d6cdf21f5
Colin Hehn <colin45h@yahoo.com>
2020-12-09 08:21:23 -0600

    Added meditation.swift + Basic UI Changes

---
+++ CLC_CalmingApp/meditation.swift
@@ -0,0 +1,22 @@
+//
+//  meditation.swift
+//  CLC_CalmingApp
+//
+//  Created by Tiger Coder on 12/9/20.
+//  Copyright © 2020 clc.hehn. All rights reserved.
+//
+
+import Foundation
+
+class meditation {
+    
+    var timeSpent : String
+    var date : String
+    
+    init(timeGo : String, dateGo : String){
+        
+        timeSpent = timeGo
+        date = dateGo
+    }
+    
+}
