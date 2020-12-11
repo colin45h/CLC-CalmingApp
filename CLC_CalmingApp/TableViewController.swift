65f3a456d315f7898d2306e0922171551e4a3018
Colin Hehn <colin45h@yahoo.com>
2020-12-11 02:07:10 -0600

    TableViewController 100% Complete and Working, Full app functionality complete

--- CLC_CalmingApp/TableViewController.swift
+++ CLC_CalmingApp/TableViewController.swift
@@ -40,14 +40,28 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = sessionView.dequeueReusableCell(withIdentifier: "session")!
         cell.textLabel?.text = stringFromDate(sessionList[indexPath.row].date)
+        cell.detailTextLabel?.text = secondsToMinutesSeconds(seconds: sessionList[indexPath.row].duration)
         return cell
     }
     
     
     // Background Functions
     func stringFromDate(_ date: Date) -> String {
+        // Converts a Date object to a readable string
         let format = DateFormatter()
         format.dateFormat = "MMM/dd/yyyy"
         return format.string(from: date)
+    }
+    
+    func secondsToMinutesSeconds(seconds: Double) -> String {
+        // Turns the amount of seconds (Double) into a readable amount of time (String)
+       let interval = seconds
+
+       let formatter = DateComponentsFormatter()
+       formatter.allowedUnits = [.hour, .minute, .second]
+       formatter.unitsStyle = .abbreviated
+
+       let formattedString = formatter.string(from: TimeInterval(interval))!
+       return formattedString
     }
 }
